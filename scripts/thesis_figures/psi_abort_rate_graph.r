dc7110d3c03611759ab1207b5fccf58a9d713a35
ergl <ergl@users.noreply.github.com>
2020-03-31 21:25:42 +0200

    Add partitions/throughput graph

--- scripts/thesis_figures/psi_abort_rate_graph.r
+++ scripts/thesis_figures/psi_abort_rate_graph.r
@@ -49,6 +49,57 @@ partitions <- ggplot(df[df$exp == "p", ]) +
 
           legend.position = "none")
 
+
+psi_color <- "#F2818F"
+partitions_throughput <- ggplot(df[df$exp == "p", ], aes(x=factor(ring),
+                                                         y=throughput,
+                                                         group=exp)) +
+
+    geom_point(size=2.5, colour=psi_color) + geom_line(colour=psi_color) +
+
+    scale_y_continuous(breaks=seq(0, 30000, 5000),
+                       expand=c(0,0),
+                       labels=function(x) format(x, big.mark = ",", scientific = FALSE)) +
+
+    labs(title="Partitions number effect on throughput",
+        subtitle="4 reads / 4 writes",
+        x="Partitions",
+        y="Throughput (tps)") +
+
+    coord_cartesian(ylim=c(0, 32500)) +
+
+    theme_minimal(base_size=10) +
+
+    theme(plot.title = element_text(color="black", size=9, hjust=1),
+          plot.subtitle = element_text(color="black", size=8, hjust=1),
+          plot.margin = margin(10,20,10,10),
+
+          axis.title.x = element_text(size=10, margin=margin(0,0,0,0)),
+          axis.title.y = element_text(size=10, margin=margin(0,5,0,10)),
+          axis.line = element_line(color="black", size=0.5),
+
+          axis.text.x = element_text(color="black", size=9, margin=margin(8,0,5,0)),
+          axis.text.y = element_text(color="black", size=9, margin=margin(0,8,0,3)),
+
+          strip.text.x = element_text(size=10),
+          strip.placement = "outside",
+          panel.spacing = unit(1, "lines"),
+
+          axis.ticks = element_line(color="black"),
+          axis.ticks.length = unit(-2.75, "pt"),
+
+          panel.grid.minor.x = element_blank(),
+          panel.grid.major.x = element_line(colour="#EBEBEB", size=0.5),
+          panel.grid.minor.y = element_blank(),
+          panel.grid.major.y = element_line(colour="#EBEBEB", size=0.5),
+
+          legend.position = "bottom",
+          legend.direction = "horizontal",
+          legend.title = element_text(size=6),
+          legend.text = element_text(size=6),
+          legend.box.just = "left",
+          legend.box.background = element_rect(color="white", fill="white"))
+
 written_keys <- ggplot(df[df$exp == "wk", ]) +
     geom_bar(aes(x=factor(write_keys), y=(1-commit_r)), fill=psi_color, colour="black", size=0.25, width=0.8, position="dodge2", stat="identity") +
     scale_y_continuous(breaks=seq(0,1,0.1), expand=c(0,0), sec.axis=dup_axis(name=NULL, labels=NULL)) +
@@ -130,5 +181,12 @@ ggsave(filename = "./out/psi_read_abort_bench.pdf",
        plot = combined,
        device = "pdf",
        width = 12,
+       height = 4,
+       dpi = 600)
+
+ggsave(filename = "./out/psi_partitions_throughput.pdf",
+       plot = partitions_throughput,
+       device = "pdf",
+       width = 5,
        height = 4,
        dpi = 600)
